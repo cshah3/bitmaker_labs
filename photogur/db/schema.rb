4beef526a2b98c325595476be42db2594b816786
cshah3 <shah.control@gmail.com>
2013-09-05 15:49:34 -0400

    refactoring

--- photogur/db/schema.rb
+++ photogur/db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130903201242) do
+ActiveRecord::Schema.define(:version => 20130905194736) do
+
+  create_table "merchants", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "pictures", :force => true do |t|
     t.string   "title"
