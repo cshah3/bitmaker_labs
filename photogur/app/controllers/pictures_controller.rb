7ab54b7313019cca07a75c086bf4728229e23e10
cshah3 <shah.control@gmail.com>
2013-09-04 20:13:01 -0400

    added all functions

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -1,48 +1,49 @@
 class PicturesController < ApplicationController
   def index
-    @pictures = [
-      {
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-      },
-      {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-      {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-      }
-    ]
+    @pictures = Picture.all
   end
 
   def show
+    @picture = Picture.find(params[:id])
   end
 
-  def picture0
-    @picture = {
-      :title  => "The old church on the coast of White sea",
-      :artist => "Sergey Ershov",
-      :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-    }
+  def new
+    @picture = Picture.new
   end
 
-  def picture1
-    @picture = {
-      :title  => "Sea Power",
-      :artist => "Stephen Scullion",
-      :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-    }
+  def create
+    # make a new picture with what picture_params returns (which is a method we're calling)
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      # if the save for the picture was successful, go to index.html.erb
+      redirect_to pictures_url
+    else
+      # otherwise render the view associated with the action :new (i.e. new.html.erb)
+      render :new
+    end
   end
 
-  def picture2
-    @picture = {
-      :title  => "Into the Poppies",
-      :artist => "John Wilhelm",
-      :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-    }
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
   end
 end
