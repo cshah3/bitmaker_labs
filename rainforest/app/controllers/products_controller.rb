58371a9b85c8fa8d0f514f995615cc50d3acb856
cshah3 <shah.control@gmail.com>
2013-09-09 10:05:48 -0400

    first commit

---
+++ rainforest/app/controllers/products_controller.rb
@@ -0,0 +1,74 @@
+class ProductsController < ApplicationController
+before_filter :get_product, :only => [:show, :edit, :update, :destroy]
+
+  def get_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
+  end
+
+  def index
+    @products = Product.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @products }
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.html
+      format.json { render json: @product }
+    end
+  end
+
+  def new
+    @product = Product.new
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @product } 
+    end   
+  end
+
+  def create
+    @product = Product.create(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render json: @product, status: :created, location: @product }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @product.update_attributes(product_params)
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end  
+  end
+
+  def destroy
+    @product.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ rainforest/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rainforest::Application.config.secret_key_base = 'c04fd10e2bc2757c9315be5522671318df7cb0e822c1e36bc772f94adeb3217b86ac754b5c266d6492604fad5fa0401d6210adaa973c675f4c2493555c445c4a'
