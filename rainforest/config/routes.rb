65721efb1d7a0cb3f3eca7408f1916b7698b0875
cshah3 <shah.control@gmail.com>
2013-09-09 15:21:16 -0400

    added session functionality

--- rainforest/config/routes.rb
+++ rainforest/config/routes.rb
@@ -2,6 +2,8 @@ Rainforest::Application.routes.draw do
   
   resources :products
   resources :users, :only => [:new, :create]
+  resources :sessions, :only => [:new, :create, :destroy]
+  
   root :to => "products#index"
   
   # The priority is based upon order of creation: first created -> highest priority.
