3c5dd8162348ec5720fd6454a85f18dead273bfc
cshah3 <shah.control@gmail.com>
2013-09-24 19:45:30 -0400

    finished variable exercises

--- javascript_fundamentals/js_fundamentals/variables.js
+++ javascript_fundamentals/js_fundamentals/variables.js
@@ -7,6 +7,13 @@
 // Store the following into variables: number of children, partner's name, geographic location, job title.
 // Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
 
+var numchild = 3
+var spouse = "Buzz"
+var city = "Tokyo"
+var job = "landscaper"
+
+alert("You will be a " + job + " in " + city + ", and married to " + spouse + " with " + numchild + " kids.")
+
 // EXERCISE: The Age Calculator
 
 // Forgot how old someone is? Calculate it!
@@ -15,6 +22,13 @@
 // Store their birth year in a variable.
 // Calculate their 2 possible ages based on the stored values.
 // Output them to the screen like so: "They are either NN or NN", substituting the values.
+
+var currentyear = 2013
+var birthyear = 1990
+var lowage = currentyear - birthyear - 1
+var highage = currentyear - birthyear
+
+alert("They are either " + lowage + " or " + highage)
 
 // EXERCISE: The Lifetime Supply Calculator
 
@@ -26,6 +40,13 @@
 // Calculate how many you would eat total for the rest of your life.
 // Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
 
+var currentage = 22
+var maxage = 100
+var daily = 5
+var total = (maxage - currentage) * 365 * daily
+
+alert("You will need " + total + " to last you until the ripe old age of " + maxage)
+
 // EXERCISE: The Geometrizer
 
 // Calculate properties of a circle, using the definitions here.
@@ -33,6 +54,13 @@
 // Store a radius into a variable.
 // Calculate the circumference based on the radius, and output "The circumference is NN".
 // Calculate the area based on the radius, and output "The area is NN".
+
+var radius = 5
+var circumference = 2 * Math.PI * radius
+var area = Math.PI * radius * radius
+
+alert("The circumference is " + circumference)
+alert("The area is " + area)
 
 // EXERCISE: The Temperature Converter
 
@@ -43,4 +71,10 @@
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output "NN°F is NN°C."
 
+var celsius = 23
+var celsfahr = celsius * (9/5) + 32
+var fahrenheit = 90
+var fahrcels = (fahrenheit - 32) * (5/9)
 
+alert(celsius + "°C is " + celsfahr + "°F")
+alert(fahrenheit + "°F is " + fahrcels + "°C")
