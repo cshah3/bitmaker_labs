b57a64f44a2378b0a9cf74023c624dfcd6ab6a7a
cshah3 <shah.control@gmail.com>
2013-09-24 22:09:29 -0400

    added array exercise

--- javascript_fundamentals/js_fundamentals/arrays.js
+++ javascript_fundamentals/js_fundamentals/arrays.js
@@ -6,5 +6,8 @@
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
+var colors = ["green", "blue", "red"]
 
-
+for (var i = 0; i < colors.length; i++) {
+	console.log("My number " + (i+1) + " choice is " + colors[i] + ".")
+}
